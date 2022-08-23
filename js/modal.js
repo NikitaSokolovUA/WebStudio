04fac49f20b52a420973b1b65b75ca37e38ec752
Nikita Sokolov UA <nsokolov0309@gmail.com>
2022-08-23 18:12:01 +0300

    Change JS
    Change JS for modal
    click on backdrop, modal is close
    click ESC on keyboard, modal is close

--- js/modal.js
+++ js/modal.js
@@ -5,10 +5,29 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", onOpenModal);
+  refs.closeModalBtn.addEventListener("click", onCloseModal);
+  refs.modal.addEventListener('click', onClickBackdropModalClose)
 
-  function toggleModal() {
+  function onOpenModal() {
+    window.addEventListener('keydown', inKeyDownEscModalClose)
     refs.modal.classList.toggle("backdrop--is-hidden");
+  }
+  function onCloseModal() {
+    window.removeEventListener('keydown',inKeyDownEscModalClose)
+    refs.modal.classList.toggle("backdrop--is-hidden");
+  }
+  
+  function onClickBackdropModalClose(event) {
+    if (event.target === event.currentTarget) {
+      onCloseModal()
+    }
+  }
+  function inKeyDownEscModalClose(event) {
+    const KEY_CODE_ESCAPE = 'Escape';
+
+    if (event.code === KEY_CODE_ESCAPE) {
+      onCloseModal()
+    }
   }
 })();
